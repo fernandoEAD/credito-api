074df49f4bdd4c8586a1b5506f948700d50122ff
Fernando Dantas <fernando-danatas@hotmail.com>
2025-07-15 19:47:51 -0300

    feat: criado entidade Credito e repository

---
+++ src/main/java/com/exemplo/credito/repository/CreditoRepository.java
@@ -0,0 +1,12 @@
+package com.exemplo.credito.repository;
+
+import com.exemplo.credito.entity.Credito;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CreditoRepository extends JpaRepository<Credito, Long> {
+    List<Credito> findByNumeroNfse(String numeroNfse);
+    Optional<Credito> findByNumeroCredito(String numeroCredito);
+}
